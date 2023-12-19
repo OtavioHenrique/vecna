7067d63c38b19e30ceef0e7223960553279a4772
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2023-12-19 19:47:01 -0300

    fix documentation

--- pkg/task/task.go
+++ pkg/task/task.go
@@ -8,6 +8,6 @@ import (
 type Task interface {
 	// Run will be called by workers (usually)
 	// it will receive context, a interface{} (usually output from previous worker) and the metadata map
-	// should always return a pointer to TaskData and error
+	// should always return a interface{} to be cast later and error
 	Run(context.Context, interface{}, map[string]interface{}, string) (interface{}, error)
 }
