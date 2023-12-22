af3a4efa6c5d66b5f21bb65c6119bbe02c7bf135
Otavio <otaviopvaladares@gmail.com>
2023-12-22 20:23:20 -0300

    Event breaker (#7)
    * Remove watcher
    
    * create event breaker worker
    
    * Examples

---
+++ pkg/workers/event_breaker_test.go
@@ -0,0 +1,68 @@
+package workers_test
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/otaviohenrique/vecna/pkg/metrics"
+	"github.com/otaviohenrique/vecna/pkg/workers"
+)
+
+func TestEventBreakerWorker_Start(t *testing.T) {
+	type fields struct {
+		name      string
+		Input     chan *workers.WorkerData
+		Output    chan *workers.WorkerData
+		numWorker int
+		logger    *slog.Logger
+		metric    metrics.Metric
+	}
+	type args struct {
+		ctx context.Context
+	}
+
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		input  []string
+	}{
+		{"It correct breaks events based on breakFn", fields{
+			name:      "test-worker",
+			Input:     make(chan *workers.WorkerData, 10),
+			Output:    make(chan *workers.WorkerData, 20),
+			numWorker: 1,
+			logger:    slog.New(slog.NewTextHandler(os.Stdout, nil)),
+			metric:    metrics.NewMockMetrics(),
+		}, args{ctx: context.TODO()}, []string{"a", "b", "c", "d"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := workers.NewEventBreakerWorker(
+				tt.fields.name,
+				tt.fields.numWorker,
+				tt.fields.logger,
+				tt.fields.metric,
+			)
+			w.Input = tt.fields.Input
+			w.Output = tt.fields.Output
+			w.Start(tt.args.ctx)
+
+			tt.fields.Input <- &workers.WorkerData{Data: tt.input, Metadata: nil}
+
+			var result []string
+			for i := 0; i < len(tt.input); i++ {
+				value := <-tt.fields.Output
+
+				result = append(result, value.Data.(string))
+			}
+
+			if reflect.DeepEqual(result, tt.input) != true {
+				t.Errorf("Should have been received all content from input on different messages. Want = %s, Received = %s", tt.input, result)
+			}
+		})
+	}
+}
