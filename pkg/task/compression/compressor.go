f8b881b60885c9b0aba9801e646e112bd52ae996
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2024-04-04 22:31:49 -0300

    Generic compression task

--- pkg/task/compression/compressor.go
+++ pkg/task/compression/compressor.go
@@ -13,14 +13,14 @@ import (
 	"github.com/klauspost/compress/zstd"
 )
 
-// adaptFn receives input from previous worker  and returns []byte to be compressed
-type CompressAdaptFn func(interface{}, map[string]interface{}) ([]byte, error)
+var (
+	ErrUnknownCompressionType = errors.New("unknown-compression-type")
+)
 
 // Compressor is a generic task capable of compress a []byte into zstd or gzip.
-type Compressor struct {
+type Compressor[T, K []byte] struct {
 	// CompressionType is either "gzip" or "zstd"
 	compressionType string
-	adaptFn         CompressAdaptFn
 	logger          *slog.Logger
 }
 
@@ -33,7 +33,7 @@ func NewWriter(compressionType string, writer io.Writer) (io.WriteCloser, error)
 	case "zstd":
 		compressor, err = zstd.NewWriter(writer)
 	default:
-		return nil, errors.New("unknown-compression-type")
+		return nil, ErrUnknownCompressionType
 	}
 
 	if err != nil {
@@ -44,31 +44,24 @@ func NewWriter(compressionType string, writer io.Writer) (io.WriteCloser, error)
 }
 
 // Creates a new Compressor task.
-func NewCompressor(compressionType string, adaptFn CompressAdaptFn, logger *slog.Logger) *Compressor {
-	d := new(Compressor)
+func NewCompressor[T, K []byte](compressionType string, logger *slog.Logger) *Compressor[T, K] {
+	d := new(Compressor[T, K])
 
 	d.compressionType = compressionType
-	d.adaptFn = adaptFn
 	d.logger = logger
 
 	return d
 }
 
 // Run receives output from previous worker, calls adaptFn and compress it into selected format.
-func (d *Compressor) Run(_ context.Context, input interface{}, meta map[string]interface{}, _ string) (interface{}, error) {
-	b, err := d.adaptFn(input, meta)
-
-	if err != nil {
-		return nil, err
-	}
-
+func (d *Compressor[T, K]) Run(_ context.Context, input T, meta map[string]interface{}, _ string) (K, error) {
 	var buf bytes.Buffer
 	zw, err := NewWriter(d.compressionType, &buf)
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = zw.Write(b)
+	_, err = zw.Write(input)
 	if err != nil {
 		return nil, err
 	}
--- pkg/task/compression/decompressor.go
+++ pkg/task/compression/decompressor.go
@@ -38,36 +38,25 @@ func NewReader(compressionType string, reader io.Reader) (io.Reader, error) {
 	return compressor, nil
 }
 
-type DecompressAdaptFn func(interface{}, map[string]interface{}) ([]byte, error)
-
 // Decompressor is a generic task capable of decompress []byte compressed with ZSTD or GZIP
-type Decompressor struct {
+type Decompressor[T, K []byte] struct {
 	// Compression type which it will focus on decompressing. Must be either zstd or gzip
 	compressionType string
-	// adaptFn which will receive interface{} from previous worker and return []byte to be decompressed
-	adaptFn DecompressAdaptFn
-	logger  *slog.Logger
+	logger          *slog.Logger
 }
 
-func NewDecompressor(compressionType string, adaptFn DecompressAdaptFn, logger *slog.Logger) *Decompressor {
-	d := new(Decompressor)
+func NewDecompressor[T, K []byte](compressionType string, logger *slog.Logger) *Decompressor[T, K] {
+	d := new(Decompressor[T, K])
 
 	d.compressionType = compressionType
-	d.adaptFn = adaptFn
 	d.logger = logger
 
 	return d
 }
 
 // The return data from Run will be []byte decompressed
-func (d *Decompressor) Run(_ context.Context, input interface{}, meta map[string]interface{}, _ string) (interface{}, error) {
-	b, err := d.adaptFn(input, meta)
-
-	if err != nil {
-		return nil, err
-	}
-
-	r := bytes.NewReader(b)
+func (d *Decompressor[T, K]) Run(_ context.Context, input T, meta map[string]interface{}, _ string) (K, error) {
+	r := bytes.NewReader(input)
 	reader, err := NewReader(d.compressionType, r)
 
 	if err != nil {
